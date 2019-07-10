ed34575a8c9605d0011193f7dd4ac426b9460a90
James M Herrington <little.chimmy@gmail.com>
2019-07-09 22:28:42 -0500

    Finding just the yellow and silver tape and then changing to black and white.

--- image-processing/find-path3.py
+++ image-processing/find-path3.py
@@ -6,13 +6,14 @@ image = cv2.imread('./2019-06-26-174012.jpg')
 
 boundaries = [
 	([160, 107, 56], [255, 203, 151]),
-	([38, 201, 199], [88, 251, 249]),
-	([138, 135, 130], [238, 235, 240])
+	([0, 163, 161], [108, 255, 255]),
+	([126, 123, 118], [238, 235, 240])
 ]
 
 	# rgb([148, 145, 140], [168, 165, 160])
 
 output2 = None
+firstImageSkipped = False
 
 for (lower, upper) in boundaries:
 	# create NumPy arrays from the boundaries
@@ -24,11 +25,30 @@ for (lower, upper) in boundaries:
 	mask = cv2.inRange(image, lower, upper)
 	output = cv2.bitwise_and(image, image, mask = mask)
  
-	if output2 is None:
-	  output2 = output
+	if firstImageSkipped == True:
+		if output2 is None:
+			output2 = output
+		else:
+			output2 = cv2.bitwise_or(output2, output)
 	else:
-		output2 = cv2.bitwise_or(output2, output)
+		firstImageSkipped = True
 
-	# show the images
-	cv2.imshow("images", np.hstack([image, output2]))
-	cv2.waitKey(0)
+# Convert to black and White
+grayImage = cv2.cvtColor(output2, cv2.COLOR_BGR2GRAY)
+(thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
+
+# show the images
+# cv2.imshow("images", output2)
+# cv2.waitKey(0)
+
+cv2.imshow("images", blackAndWhiteImage)
+cv2.waitKey(0)
+
+# 86, 467
+# 366, 63
+
+
+# source = to_index(86, 467)
+# target = to_index(366, 63)
+
+
